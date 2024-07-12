f84f642075beed7c3df50fd6cd6becf307b6e0c6
zombkit <stephen.korzen@gmail.com>
2024-07-12 03:50:15 -0700

    dfg

--- scratch_work/slov_common/src/components.rs
+++ scratch_work/slov_common/src/components.rs
@@ -46,6 +46,9 @@ impl GameRenderable {
 }
 
 
+// for horse vehicle use horse chess piece as head  ∧ ♞
+
+// projectile weapons have two parts (path to take, and steps of path to take, something flying slow and not far will have low each, something flying slow but far will ahve lots of steps, this can be passed to Go()
 //MAKE IT POSSIBLE TO SET PLAYER AI MODE FOR AUTO PLAYING
 #[derive(Clone, Debug, PartialEq, Component)]
 pub enum ActionComponent {
