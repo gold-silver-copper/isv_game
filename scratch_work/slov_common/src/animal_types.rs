8f3fce862a852b7e1dee97fbe9559b298980889e
zombkit <stephen.korzen@gmail.com>
2024-07-17 19:21:26 -0700

    uio

--- scratch_work/slov_common/src/animal_types.rs
+++ scratch_work/slov_common/src/animal_types.rs
@@ -36,6 +36,7 @@ pub enum SubspeciesAdjective {
     Familiaris,
     Domesticus,
     Minor,
+    Major,
     Aureus,
     Unicus,
     Vulgatis,
@@ -45,6 +46,17 @@ pub enum SubspeciesAdjective {
     Barbarus,
     Timidus,
     Alpinus,
+    Islandicus,
+    Obscurus,
+    Acadicus,
+    Magnus,
+    Agilis,
+    Flavus,
+    Superus,
+    Arboreus,
+    Velox,
+
+    Pacificus,
 }
 
 #[derive(Clone, Debug, Display, PartialEq)]
@@ -58,17 +70,218 @@ pub enum Animalia {
 }
 
 #[derive(Clone, Debug, Display, PartialEq)]
+pub enum Pisces {
+    Apodes,
+    Jugulares,
+    Thoracici,
+    Abdominales,
+    Branchiotegi,
+    Chondropterygii,
+}
+
+#[derive(Clone, Debug, Display, PartialEq)]
+pub enum Apodes {
+    Muraena,
+    Gymnotus,
+    Anarchicas,
+    Trichiurus,
+    Ammodytes,
+    Ophidium,
+    Stromateus,
+    Xiphias,
+}
+#[derive(Clone, Debug, Display, PartialEq)]
+pub enum Jugulares {
+    Callionymus,
+    Uranoscopus,
+    Trachinus,
+    Gadus,
+    Blennius,
+    Kurtus,
+}
+#[derive(Clone, Debug, Display, PartialEq)]
+pub enum Thoracici {
+    Cepola,
+    Echeneis,
+    Coryphaena,
+    Gobius,
+    Cottus,
+    Scorphaena,
+    Zeus,
+    Pleuronectes,
+    Chaetodon,
+    Sparus,
+    Scarus,
+    Labrus,
+    Sciaena,
+    Perca,
+    Gasterosteus,
+    Scomber,
+    Centrogaster,
+    Mullus,
+    Trigla,
+}
+#[derive(Clone, Debug, Display, PartialEq)]
+pub enum Abdominales {
+    Cobitis,
+    Amia,
+    Silurus,
+    Teuthis,
+    Loricaria,
+    Salmo,
+    Fistularia,
+    Esox,
+    Elops,
+    Argentina,
+    Atherina,
+    Mugil,
+    Exocoetus,
+    Polynemus,
+    Clupea,
+    Cyprinus,
+}
+#[derive(Clone, Debug, Display, PartialEq)]
+pub enum Branchiostegi {
+    Mormyrus,
+    Ostracion,
+    Tetrodon,
+    Diodon,
+    Syngnathus,
+    Pegasus,
+    Centriscus,
+    Balistes,
+    Cyclopterus,
+    Lophius,
+}
+#[derive(Clone, Debug, Display, PartialEq)]
+pub enum Chondropterygii {
+    Acipenser,
+    Chimaera,
+    Squalus,
+    Raia,
+    Petromyzon,
+}
+
+#[derive(Clone, Debug, Display, PartialEq)]
+pub enum Amphibia {
+    Reptilia,
+    Serpentes,
+}
+
+#[derive(Clone, Debug, Display, PartialEq)]
+pub enum Reptilia {
+    Testudo,
+    Draco,
+    Lacerta,
+    Rana,
+    Crocodilus,
+    Alligator,
+    Dracaena,
+    Iguana,
+    Salamander,
+    Gecko,
+    Chamaeleon,
+}
+
+#[derive(Clone, Debug, Display, PartialEq)]
+pub enum Serpentes {
+    Crotalus,
+    Boa,
+    Constrictor,
+    Vipera,
+    Scytha,
+    Cobra,
+    Aspis,
+
+    Coluber,
+    Anguis,
+    Amphisbaena,
+    Caecilia,
+}
+
+#[derive(Clone, Debug, Display, PartialEq)]
 pub enum Aves {
     Accipitres, //hawk
     Picae,      //magpie
     Anseres,    //gooese
-    Grallae,    //stilt
-    Gallinae,
-    Passeres,
+    Grallae,    //stilt like flamingo
+    Gallinae,   //chicken
+    Passeres,   //songbird
 }
 
 #[derive(Clone, Debug, Display, PartialEq)]
-pub enum Pica {
+pub enum Grallae {
+    Phoenicopterus, //flamingo
+    Platalea,
+    Palamedea,
+    Mycteria,
+    Tantalus,
+    Ardea,
+    Recurvirostra,
+    Scolopax,
+    Tringa,
+    Fulica,
+    Parra,
+    Rallus,
+    Psophia,
+    Cancroma,
+    Haematopus,
+    Charadricus,
+}
+
+#[derive(Clone, Debug, Display, PartialEq)]
+pub enum Gallinae {
+    Otis,
+    Struthio,
+    Didus,
+    Pavo,
+    Meleagris,
+    Penelope,
+    Crax,
+    Phasianus,
+    Numida,
+    Tetrao,
+}
+
+#[derive(Clone, Debug, Display, PartialEq)]
+pub enum Passeres {
+    Loxia,
+    Colius,
+    Fringilla,
+    Phytotoma,
+    Emberiza,
+    Caprimulgus,
+    Hirundo,
+    Pipra,
+    Ampelis,
+    Tanagra,
+    Muscicapa,
+    Parus,
+    Motacilla,
+    Alauda,
+    Sturnus,
+    Columba,
+}
+
+#[derive(Clone, Debug, Display, PartialEq)]
+pub enum Anseres {
+    Anas,
+    Mergus,
+    Phaeton,
+    Plotus,
+    Rhyncops,
+    Diomedea,
+    Aptenodyta,
+    Alca,
+    Procellaria,
+    Pelecanus,
+    Larus,
+    Sterna,
+    Colymbus,
+}
+
+#[derive(Clone, Debug, Display, PartialEq)]
+pub enum Picae {
     Trochilus,
     Certhia,
     Upupa,
@@ -82,14 +295,28 @@ pub enum Pica {
 
     Corvus,
     Paradisea,
+    Ramphastos,
+    Trogon,
+    Psittacus,
+    Crotophaga,
+    Picus,
+    Yunx,
+    Cuculus,
+    Bucco,
+    Buceros,
+    Alcedo,
+    Merops,
+    Todus,
 }
 
 #[derive(Clone, Debug, Display, PartialEq)]
-pub enum Accipiter {
+pub enum Accipitres {
     Falco,
     Vultur,
     Strix,
     Lanius,
+    Gryphus,
+    Harpyia,
 }
 
 #[derive(Clone, Debug, Display, PartialEq)]
