52c030a4abd79b6e5990c92c7187f44c691cf90f
zombkit <stephen.korzen@gmail.com>
2024-08-19 10:39:32 -0700

    f

--- scratch_work/tui_v/src/typedefs.rs
+++ scratch_work/tui_v/src/typedefs.rs
@@ -22,10 +22,7 @@ pub type GraphicTriple = (&'static str, RatColor, RatColor);
 
 pub struct GraphicComponent(pub GraphicTriple);
 
-// for horse vehicle use horse chess piece as head  ∧ ♞
 
-// projectile weapons have two parts (path to take, and steps of path to take, something flying slow and not far will have low each, something flying slow but far will ahve lots of steps, this can be passed to Go()
-//MAKE IT POSSIBLE TO SET PLAYER AI MODE FOR AUTO PLAYING
 #[derive(Clone, Debug, PartialEq)]
 pub enum GameAction {
     Wait,
